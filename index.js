2f74879c48fc6169a5e1f9bfb5e12fff58516415
Ana Nikolova <ani.nik19@gmail.com>
2020-06-30 10:54:43 +0200

    vegeta time

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+app.use(express.static("static"));
 
 app.get("/", (req, res) => {
   res.send("<h1>It is working!</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send({ page: "About Page" });
+});
+
+app.get("/gallery", (req, res) => {
+  res.send("<h1>Gallery</h1><div><img src='/img/vegeta.jpeg'/></div>");
 });
 
 app.listen(port, () => console.log(`Surver is running on port ${port}`));
