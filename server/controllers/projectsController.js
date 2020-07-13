f82986db5086ad5ac03a4b6115a4c064515f992d
Ana Nikolova <ani.nik19@gmail.com>
2020-07-13 11:06:49 +0200

    seeting the routes

--- server/controllers/projectsController.js
+++ server/controllers/projectsController.js
@@ -5,4 +5,27 @@ projectsConroller.index = (req, res) => {
   res.render("projects/index");
 };
 
+projectsConroller.create = (req, res) => {
+  res.render("projects/index");
+};
+
+projectsConroller.store = (req, res) => {
+  res.render("projects/index");
+};
+
+projectsConroller.show = (req, res) => {
+  res.render("projects/index");
+};
+
+projectsConroller.edit = (req, res) => {
+  res.render("projects/index");
+};
+projectsConroller.update = (req, res) => {
+  res.render("projects/index");
+};
+
+projectsConroller.destroy = (req, res) => {
+  res.render("projects/index");
+};
+
 module.exports = projectsConroller;
--- server/routes/web.js
+++ server/routes/web.js
@@ -4,14 +4,32 @@ const pagesController = require("../controllers/pagesController");
 const projectsController = require("../controllers/projectsController");
 const blogController = require("../controllers/blogController");
 
+// ================================================
+// Pages
+//=================================================
 router.get("/", pagesController.homePage);
-
 router.get("/about", pagesController.aboutPage);
+router.get("/contact", pagesController.contact);
 
+// ================================================
+// Projects
+//=================================================
 router.get("/projects", projectsController.index);
+router.get("/projects/create", projectsController.create);
+router.post("/projects", projectsController.store);
+router.get("/projects/:id", projectsController.show);
+router.get("/projects/:id/edit", projectsController.edit);
+router.put("/projects/:id/edit", projectsController.update);
+router.delete("/projects/:id", projectsController.destroy);
 
+// ================================================
+// Blog
+//=================================================
 router.get("/blog", blogController.index);
 
-router.get("/contact", pagesController.contact);
+// ================================================
+// Admin
+//=================================================
+router.get("/admin", blogController.index);
 
 module.exports = router;
